a195a38038d7bddf03824dfb36e8f684fe8ed25a
Anchalhdpandey <152679483+Anchalhdpandey@users.noreply.github.com>
2024-06-12 01:16:23 +0530

    fe

---
+++ my-react-app/src/Components/DescriptionBox/Description.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Description.css";
+const Description = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="description-nav-box">Description</div>
+        <div className="description-nav-box-fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionBox-description">
+        <p>
+          Myntra is a one stop shop for all your fashion and lifestyle needs.
+          Being India's largest e-commerce store for fashion and lifestyle
+          products, Myntra aims at providing a hassle free and enjoyable
+          shopping experience to shoppers across the country with the widest
+          range of brands and products on its portal. The brand is making a
+          conscious effort to bring the power of fashion to shoppers with an
+          array of the latest and trendiest products available in the country.
+        </p>
+        <p>
+
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Description;
--- my-react-app/src/Pages/Product.jsx
+++ my-react-app/src/Pages/Product.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Components/Context/ShopContext'
+import { useParams } from 'react-router-dom';
+import Breadcrums from '../Components/Breadcrums/Breadcrums';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
+import Description from '../Components/DescriptionBox/Description';
+import RelatedProducts from '../Components/RelatedProducts/RelatedProducts';
 
 const Product = () => {
+  const {all_product}=useContext(ShopContext);
+  const {productId}=useParams();
+  const product=all_product.find((e)=>e.id===Number(productId))
   return (
-    <div>Product</div>
+    <div>
+      <Breadcrums product={product}/>
+      <ProductDisplay product={product}/>
+      <Description/>
+      <RelatedProducts/>
+    </div>
   )
 }
 
